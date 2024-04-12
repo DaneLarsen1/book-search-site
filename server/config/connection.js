f93b178b98079800db9e1bc91a4b1a6c85d09b04
DaneLarsen1 <danelarsen12@gmail.com>
2024-04-11 19:57:16 -0600

    trying to make render work

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
+// require("dotenv").config();
 const mongoose = require('mongoose');
+// const uri = "mongodb+srv://danelarsen12:Familyfun1@cluster0.egapl0s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-
 module.exports = mongoose.connection;
